f18716216d993ddb3b77bd805ace15ecebb11722
yagamiram <sriram.ssnit@gmail.com>
2015-12-01 01:47:54 -0600

    New Algo : Word frequency
    The alogrithm is a straight forward one.

--- fractal_template.py
+++ fractal_template.py
@@ -116,15 +116,57 @@ class fractalSummary:
                 if quota_count < self.quota:
                     self.summary.append(each_sen.strip())
         print quota_sum
+class WordFrequency:
+    def __init__(self, text, quota):
+        self.text = text
+        self.summary = []
+        self.sentenceIndex = 0
+        self.quota = quota
+    def calculate_relative_frequence(self, sentence_token, weights):
+        frequency = {}
+        for each_word, freq in weights.items():
+            if each_word in sentence_token:
+                frequency[each_word] = freq
+        return frequency
+    def summarize(self):
+        punkt_param = PunktParameters()
+        punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc'])
+        sentence_splitter = PunktSentenceTokenizer(punkt_param)
+        sentences = sentence_splitter.tokenize(self.text)
+        structure = {}
+        sentence_objects = []
+        for idx in range(len(sentences)):
+            obj = {'text' : sentences[idx], 'index' : idx , 'data': {}}
+            sentence_objects.append(obj)
+        structure['sentences'] = sentence_objects
+        structure['ordered'] = []
+        structure['weights'] = {'words' : FreqDist(nltk.word_tokenize(preprocess(self.text))), 'total': 0}
+        structure['weights']['total'] = sum(structure['weights']['words'].values())
+        for each_sent in structure['sentences']:
+            each_sent['data']['tokens'] = nltk.word_tokenize(preprocess(each_sent['text']))
+            each_sent['data']['weights'] = self.calculate_relative_frequence(each_sent['data']['tokens'], structure['weights']['words'])
+        #structure['ordered'] = structure['sentences'].sort(key=lambda x:x['data']['weights'], reverse=True)
+        structure['ordered'] = sorted(structure['sentences'], key=lambda x:x['data']['weights'], reverse=True)
+        structure_keep = structure['ordered'][:self.quota]
+        structure_keep.sort(key=lambda x:x['index'])
+        for eac_sen in structure_keep:
+            self.summary.append(eac_sen['text'])
 def main():
     f = open("news.txt","r")
     #print type(f.read())
-    fs = fractalSummary(f.read(), 8)
+    text = f.read()
+    fs = fractalSummary(text, 6)
     fs.fractal_representation()
     fs.normalize()
     print "summary"
     for eac_sen in fs.summary:
         print eac_sen
     #print fs.quota_sum
+    print "word frequency"
+    wf = WordFrequency(text, 6)
+    wf.summarize()
+    print "summary"
+    for each_sen in wf.summary:
+        print each_sen
 if __name__ == "__main__":
     main()
